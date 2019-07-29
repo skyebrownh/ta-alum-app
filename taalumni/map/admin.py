d12a1355f13ea8bd1a41ba3e39749eb42ca3dba3
Skye Brown <skye.brown@uky.edu>
2019-07-29 15:35:24 -0400

    add Location DB, Member location computed field for geocoding optimization

--- taalumni/map/admin.py
+++ taalumni/map/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Member
+from .models import Member, Location
 
 # Register your models here.
 admin.site.register(Member)
+admin.site.register(Location)
--- taalumni/map/models.py
+++ taalumni/map/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+import googlemaps
+from .constants import API_KEY
+
 class Member(models.Model):
   first_name = models.CharField(max_length=30)
   last_name = models.CharField(max_length=30)
@@ -8,9 +11,40 @@ class Member(models.Model):
   # location
   city = models.CharField(max_length=60)
   state = models.CharField(max_length=30) # state abbreviation
-  # TODO: image
+  latitude = models.DecimalField(max_digits=12, decimal_places=7, editable=False)
+  longitude = models.DecimalField(max_digits=12, decimal_places=7, editable=False)
   email = models.EmailField()
   linkedin = models.URLField(blank=True)
+  # TODO: image
 
   def __str__(self):
     return "{} {}".format(self.first_name, self.last_name)
+
+  def save(self):
+    # either get city and state from Location DB or geocode and add new entry to Location DB
+    try:
+      potential_location = Location.objects.get(city=self.city, state=self.state)
+      self.latitude = potential_location.latitude
+      self.longitude = potential_location.longitude
+    except Location.DoesNotExist:
+      # geocode this member's city and state
+      gmaps = googlemaps.Client(key="{}".format(API_KEY))
+      geocode_result = gmaps.geocode("{}, {}".format(self.city, self.state))
+      # set lat and long for this member to result of geocode
+      self.latitude = geocode_result[0]["geometry"]["location"]["lat"]
+      self.longitude = geocode_result[0]["geometry"]["location"]["lng"]
+      # create new Location entry
+      Location(city=self.city, state=self.state, latitude=self.latitude, longitude=self.longitude).save()
+    # call normal save method on this object
+    super(Member, self).save()
+
+
+class Location(models.Model):
+  # DB starts empty and gets populated as Members are added
+  city = models.CharField(max_length=60)
+  state = models.CharField(max_length=30)
+  latitude = models.DecimalField(max_digits=12, decimal_places=7, editable=False)
+  longitude = models.DecimalField(max_digits=12, decimal_places=7, editable=False)
+
+  def __str__(self):
+    return "{}, {}: {}, {}".format(self.city, self.state, self.latitude, self.longitude)
