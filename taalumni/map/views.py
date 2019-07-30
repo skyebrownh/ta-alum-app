f8dd7da7cbf97ad776971a652f748dbc6bf4a28f
Skye Brown <skye.brown@uky.edu>
2019-07-30 12:41:32 -0400

    add leaflet.markercluster for handling duplicate member locations

--- taalumni/map/views.py
+++ taalumni/map/views.py
@@ -3,14 +3,19 @@ from django.shortcuts import render
 from .models import Member
 
 def mapView(request):
-  num_members = Member.objects.count()
+  num_members = Member.objects.latest('pk').id
   context = {
     'num_members': num_members,
     'members': {},
+    'deleted_members': [],
   }
   # populate context.members
   for num in range(1, num_members + 1):
-    context["members"]["member_{}".format(num)] = {
-      'object': Member.objects.get(pk=num)
-    }
+    try:
+      context["members"]["member_{}".format(num)] = {
+        'object': Member.objects.get(pk=num)
+      }
+    except Member.DoesNotExist:
+      context["deleted_members"].append(num)
+      continue
   return render(request, 'map/map.html', context)
