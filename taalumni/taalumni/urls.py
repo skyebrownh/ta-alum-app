73afcfb621b6878991973bdbd066b781c75ab576
Skye Brown <skye.brown@uky.edu>
2019-07-30 14:44:45 -0400

    new 'home' app that renders landing-page template

--- taalumni/taalumni/urls.py
+++ taalumni/taalumni/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from home import views
+
 urlpatterns = [
+    path('', views.landingPage),
     path('map/', include('map.urls')),
     path('admin/', admin.site.urls),
 ]
