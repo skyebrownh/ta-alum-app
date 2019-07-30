71edd10900b03d8c8409ec91782fadb27a7be969
Skye Brown <skye.brown@uky.edu>
2019-07-30 14:34:36 -0400

    remove base views.py and reset urls

--- taalumni/taalumni/urls.py
+++ taalumni/taalumni/urls.py
@@ -16,10 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-from . import views
-
 urlpatterns = [
-    path('', views.landingPage),
     path('map/', include('map.urls')),
     path('admin/', admin.site.urls),
 ]
