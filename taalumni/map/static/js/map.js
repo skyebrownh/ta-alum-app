c632979a3d103ce55676200053952c19e3248e66
Skye Brown <skye.brown@uky.edu>
2019-08-05 13:18:39 -0400

    fix Member looping for displaying map markers

--- taalumni/map/static/js/map.js
+++ taalumni/map/static/js/map.js
@@ -1,8 +1,8 @@
 // DOM constants
-const memberGroup = document.querySelector('.member-group');
+const memberGroupList = document.querySelector('.member-group>ul');
 
 // instantiate map object
-const map = L.map('mapid').setView([38.0406, -84.5037], 5);
+const map = L.map('mapid').setView([38.0406, -84.5037], 4);
 
 // add tile layer
 L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=bZBPrAxMkP6v1Se4TGyK', {
@@ -18,36 +18,36 @@ const icon = L.icon({
 // create marker cluster to group duplicate/geographically close markers
 const markerCluster = L.markerClusterGroup();
 
+// get collection of memberGroupList child divs
+const memberCollection = memberGroupList.getElementsByClassName('popup-info');
+
 // add marker and get popup info for each member
-for (let i = 1; i <= num_members; i++) {
-  // only continue if this member is not in deleted_members
-  if (!deleted_members.includes(i)) {
-    // get this member's popup
-    const popupInfo = memberGroup.querySelector(`#member_${i}`);
+for (let i = 0; i < memberCollection.length; i++) {
+  // get member popup
+  const popupInfo = memberCollection[i];
 
-    // get lat, long and populate location to display marker
-    const latlong = popupInfo.querySelector('.latlong');
-    const latitude = parseInt(latlong.querySelector('#lat').textContent);
-    const longitude = parseInt(latlong.querySelector('#long').textContent);
-    
-    // use latitude, longitude for marker location
-    const marker = L.marker([latitude, longitude], {
-      icon: icon,
-      title: `${memberGroup.querySelector('.location-point')}`,
-      alt: `${memberGroup.querySelector('.location-point')}`,
-      riseOnHover: true,
-      riseOffset: 250
-    });
+  // get lat, long and populate location to display marker
+  const latlong = popupInfo.querySelector('.latlong');
+  const latitude = parseInt(latlong.querySelector('#lat').textContent);
+  const longitude = parseInt(latlong.querySelector('#long').textContent);
+  
+  // use latitude, longitude for marker location
+  const marker = L.marker([latitude, longitude], {
+    icon: icon,
+    title: `${popupInfo.querySelector('.location-point')}`,
+    alt: `${popupInfo.querySelector('.location-point')}`,
+    riseOnHover: true,
+    riseOffset: 250
+  });
 
-    // add to marker cluster
-    markerCluster.addLayer(marker);
+  // add to marker cluster
+  markerCluster.addLayer(marker);
 
-    const boundMarker = marker.bindPopup(popupInfo);
+  const boundMarker = marker.bindPopup(popupInfo);
 
-    marker.addEventListener('click', () => {
-      boundMarker.openPopup();
-    });
-  }
+  marker.addEventListener('click', () => {
+    boundMarker.openPopup();
+  });
 }
 
 map.addLayer(markerCluster);
