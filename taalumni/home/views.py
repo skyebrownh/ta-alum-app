189b9a62e2adbc3b5d70ef6fa20f9daf2636c3a9
Skye Brown <skye.brown@uky.edu>
2019-08-06 13:46:59 -0400

    display member profile photo from google cloud storage

--- taalumni/home/views.py
+++ taalumni/home/views.py
@@ -1,10 +1,4 @@
 from django.shortcuts import render
 
-from google.cloud import storage
-
 def landingPage(request):
-  client = storage.Client()
-  bucket = client.get_bucket('team-alpha-alumni')
-  skye_photo = bucket.get_blob('member_images/Skye.jpg')
-
-  return render(request, 'home/landing-page.html', {'skye_photo': skye_photo.public_url})
+  return render(request, 'home/landing-page.html')
