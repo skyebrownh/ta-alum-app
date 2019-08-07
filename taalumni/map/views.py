53272eb248ff33b6e38d585828c9b833bba8c736
Skye Brown <skye.brown@uky.edu>
2019-08-07 11:13:33 -0400

    Revert "Merge pull request #2 from skyebrownh/FT/google-storage"
    This reverts commit e9a04fe30a0f868a18d81bcf3c979c1652c9f078, reversing
    changes made to 2e5cedf29a5d20212e48623c1801dc25c472a98a.

--- taalumni/map/views.py
+++ taalumni/map/views.py
@@ -2,20 +2,12 @@ from django.shortcuts import render
 
 from .models import Member
 
-from google.cloud import storage
-
 def mapView(request):
-  # ensure GOOGLE_APPLICATION_CREDENTIALS env variable is set to .json file that holds auth key
-  client = storage.Client()
-  bucket = client.get_bucket('team-alpha-alumni')
-  skye_photo = bucket.get_blob('member_images/Skye.jpg')
-
   num_members = Member.objects.latest('pk').id
   context = {
     'num_members': num_members,
     'members': {},
     'deleted_members': [],
-    'skye_photo': skye_photo.public_url,
   }
   # populate context.members
   for num in range(1, num_members + 1):
