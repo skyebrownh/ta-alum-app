f4837476ed15ea3b8e3456a0866987c1aeed925e
Skye Brown <skye.brown@uky.edu>
2019-08-07 11:16:25 -0400

    Revert "display member profile photo from google cloud storage"
    This reverts commit 94e9067a9b16266b0b22e88f1f7244c16f4544f1.

--- taalumni/home/views.py
+++ taalumni/home/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
+from google.cloud import storage
+
 def landingPage(request):
-  return render(request, 'home/landing-page.html')
+  client = storage.Client()
+  bucket = client.get_bucket('team-alpha-alumni')
+  skye_photo = bucket.get_blob('member_images/Skye.jpg')
+
+  return render(request, 'home/landing-page.html', {'skye_photo': skye_photo.public_url})
