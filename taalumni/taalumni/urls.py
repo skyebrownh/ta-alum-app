4d13541cf9f1847ed3ce21669f10df7b4123d9d4
Skye Brown <skye.brown@uky.edu>
2019-08-14 13:38:54 -0400

    remove home app and refactor as project view/template

--- taalumni/taalumni/urls.py
+++ taalumni/taalumni/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-from home import views
+from . import views
 
 urlpatterns = [
     path('', views.landingPage, name='landing-page'),
